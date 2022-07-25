dfb4d58897a36207b189f03103a8ab25c3725820
KevinBae00 <109911321+KevinBae00@users.noreply.github.com>
2022-07-25 14:28:05 +0900

    Update week4_2.cpp

--- coding_test/week4/week4_2.cpp
+++ coding_test/week4/week4_2.cpp
@@ -2,14 +2,14 @@
 using namespace std;
 
 int main(){
-    int n, H, W, X;
+    int n, H, W, X;//n: 명령 입력 갯수, H: 총 층수, W: 총 호수, X:몇 번째 손님인지
     int a,b;
     cin >> n;
     for(int i=0; i< n; i++){
         cin >> H >> W >> X;
         a=X%H;
         b=X/H+1;
-        if(X<1||X>H*W){
+        if(X<1||X>H*W){//방을 초과할 때
             cout<< "no room please enter again"<<endl;
             i--;
             continue;
