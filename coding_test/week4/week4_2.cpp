6cb24d12d31071cc08d2294ea19182e1518e78ef
KevinBae00 <109911321+KevinBae00@users.noreply.github.com>
2022-07-26 16:15:03 +0900

    ACM호텔

--- coding_test/week4/week4_2.cpp
+++ coding_test/week4/week4_2.cpp
@@ -7,16 +7,17 @@ int main(){
     cin >> n;
     for(int i=0; i< n; i++){
         cin >> H >> W >> X;
-        a=X%H;
-        b=X/H+1;
+        a= (X%H)==0 ? H:X%H;//H*n층일 때 X%H 은 0이 된다.
+        b=(X%H)==0 ? X/H:(X/H+1);////H*n층일 때 X/H의 값이 1 추가된다.
         if(X<1||X>H*W){//방을 초과할 때
             cout<< "no room please enter again"<<endl;
             i--;
-            continue;
         }
-        if(b<10)
-            cout <<a<<"0"<<b<<endl;
-        else
-            cout <<a<<b<<endl;
+        else {
+            if(b<10)
+                cout <<a<<"0"<<b<<endl;
+            else
+                cout <<a<<b<<endl;
+        }
     }
 }
